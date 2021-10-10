3e03bf95fe863b2f674848d202e36948b3ebd5d1
Devan Pellow <devanpellow@gmail.com>
2021-10-10 17:10:02 +0200

    playing with font sizes

--- src/components/moaas.js
+++ src/components/moaas.js
@@ -3,10 +3,12 @@ import React from 'react';
 export default function Moaas() {
   return (
     <div class="flex flex-col items-center my-10 md:my-20">
-      <h2 class="text-gray-500 text-3xl sm:text-xl md:text-3xl mb-4 text-center">MOASS?</h2>
+      <h2 class="text-gray-500 sm:text-xl md:text-2xl mb-4 text-center">MOASS?</h2>
       <h1 class="text-red-500 text-5xl sm:text-7xl font-extrabold md:text-8xl md:font-black text-center">
         NOT YET
       </h1>
+      <p class="text-gray-500 text-xl md:text-2xl my-8">Keep 💎🙌</p>
+      <p class="text-gray-500 text-xs my-8">This is not financial advice. I don't know anything 🥚.</p>
     </div>
   );
 }

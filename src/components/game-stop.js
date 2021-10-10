672e3d3061cee99396f6633da797f29b103c28d8
Devan Pellow <devanpellow@gmail.com>
2021-10-10 16:18:32 +0200

    playing with font sizes

--- src/components/game-stop.js
+++ src/components/game-stop.js
@@ -18,7 +18,7 @@ export default function GameStop() {
   return (
     <div class="flex flex-col items-center">
       <h1 class="text-red-500 text-6xl font-black">GME</h1>
-      <h1 class="text-white text-7xl md:text-9xl mt-10 md:mt-20">${price}</h1>
+      <h1 class="text-white text-7xl md:text-8xl mt-10 md:mt-20">${price}</h1>
     </div>
   );
 }
--- src/components/moaas.js
+++ src/components/moaas.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Moaas() {
   return (
     <div class="flex flex-col items-center my-10 md:my-20">
-      <h2 class="text-gray-500 text-3xl sm:text-4xl md:text-6xl mb-4 text-center">Did the MOASS happen?</h2>
-      <h1 class="text-red-500 text-5xl sm:text-7xl font-extrabold md:text-9xl md:font-black text-center">
+      <h2 class="text-gray-500 text-3xl sm:text-xl md:text-3xl mb-4 text-center">MOASS?</h2>
+      <h1 class="text-red-500 text-5xl sm:text-7xl font-extrabold md:text-8xl md:font-black text-center">
         NOT YET
       </h1>
     </div>
